77c106fcdbfddb23690d3bbfec000108c81a1fdd
Paweł Mikołaj Gryka <p.gryka@stud.elka.pw.edu.pl>
2021-01-05 16:52:15 +0100

    Another initial commit

---
+++ DomKultury/src/main/java/bada_proi/DomKultury/DomKulturyApplication.java
@@ -0,0 +1,13 @@
+package bada_proi.DomKultury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomKulturyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomKulturyApplication.class, args);
+	}
+
+}
---
+++ DomKultury/src/test/java/bada_proi/DomKultury/DomKulturyApplicationTests.java
@@ -0,0 +1,13 @@
+package bada_proi.DomKultury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomKulturyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
