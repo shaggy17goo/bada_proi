4293176abcd7bb983a4d414e3474ebc6c5f8af51
Paweł Mikołaj Gryka <p.gryka@stud.elka.pw.edu.pl>
2021-01-15 19:28:25 +0100

    Re-add unit tests

---
+++ src/test/java/bada_proi/DomKultury/CulturalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package bada_proi.DomKultury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
