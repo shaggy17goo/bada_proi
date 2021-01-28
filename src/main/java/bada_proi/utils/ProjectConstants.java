ce8623d088fb746893fd07e1321e6d344be63459
Michał Wawrzyńczak <m.wawrzynczak@stud.elka.pw.edu.pl>
2021-01-28 15:57:50 +0100

    Make the realizationInfoPage prettier

--- src/main/java/bada_proi/utils/ProjectConstants.java
+++ src/main/java/bada_proi/utils/ProjectConstants.java
@@ -2,9 +2,9 @@ package bada_proi.utils;
 
 public class ProjectConstants {
 
-    public static final String URL = "jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
     public static final String USERNAME = "mwawrzyn";
-    public static final String PASSWORD = "mwawrzyn";
+    public static final String PASSWORD = "Argax6ALWlFS9P5J";
     public static final String INFO = "INFO";
     public static final String DRIVERCLASSNAME = "oracle.jdbc.OracleDriver";
 
